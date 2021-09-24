4c4070ade7658d02818ef23d27fd806c9809d684
Matej Skalicky <matej.skalickyy@gmail.com>
2021-09-24 11:04:35 +0200

    upravy

--- permutace.py
+++ permutace.py
@@ -1,4 +1,24 @@
 import math
+from pywebio.input import input, FLOAT
+from pywebio.output import put_text
+
+def bmi():
+    height = input("Input your height(cm)：", type=FLOAT)
+    weight = input("Input your weight(kg)：", type=FLOAT)
+
+    BMI = weight / (height / 100) ** 2
+
+    top_status = [(16, 'Severely underweight'), (18.5, 'Underweight'),
+                  (25, 'Normal'), (30, 'Overweight'),
+                  (35, 'Moderately obese'), (float('inf'), 'Severely obese')]
+
+    for top, status in top_status:
+        if BMI <= top:
+            put_text('Your BMI: %.1f. Category: %s' % (BMI, status))
+            break
+
+if __name__ == '__main__':
+    bmi()
 
 while 1:
     print("Zvol si operaci\nPro operaci stiskni číslo:\n\nPermutace\t\t1\nVariace\t\t\t2\nKombinace\t\t3\nSvetry ve výloze\t4\nVariace s opakováním\t5\n") #Pro ukončení napiš: konec\n
@@ -35,9 +55,9 @@ while 1:
                     n = int(input("Zadej n: "))
                     k = int(input("Zadej počet prvků: "))
                     print("\nVýsledek je: \t", n ** k)
-                #elif o == "konec":
-                    #print("konec")
-                    #break
+                elif o == "konec":
+                    print("konec")
+                    break
                 else:
                     print("Cyba ve zadání operace")
             except:
