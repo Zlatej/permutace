75c9cde2222a524e92f95cc34d5767cd258fc5c0
Matej Skalicky <matej.skalickyy@gmail.com>
2021-10-03 10:32:16 +0200

    ntb nevim

--- permutace.py
+++ permutace.py
@@ -1,24 +1,4 @@
 import math
-from pywebio.input import input, FLOAT
-from pywebio.output import put_text
-
-def bmi():
-    height = input("Input your height(cm)：", type=FLOAT)
-    weight = input("Input your weight(kg)：", type=FLOAT)
-
-    BMI = weight / (height / 100) ** 2
-
-    top_status = [(16, 'Severely underweight'), (18.5, 'Underweight'),
-                  (25, 'Normal'), (30, 'Overweight'),
-                  (35, 'Moderately obese'), (float('inf'), 'Severely obese')]
-
-    for top, status in top_status:
-        if BMI <= top:
-            put_text('Your BMI: %.1f. Category: %s' % (BMI, status))
-            break
-
-if __name__ == '__main__':
-    bmi()
 
 while 1:
     print("Zvol si operaci\nPro operaci stiskni číslo:\n\nPermutace\t\t1\nVariace\t\t\t2\nKombinace\t\t3\nSvetry ve výloze\t4\nVariace s opakováním\t5\n") #Pro ukončení napiš: konec\n
