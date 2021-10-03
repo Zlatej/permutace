f7b2e1c4de3b3a63cbe12d744f3297b65c58b4e6
Matej Skalicky <matej.skalickyy@gmail.com>
2021-10-03 21:10:27 +0200

    flask faze 2

--- app.py
+++ app.py
@@ -6,33 +6,42 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-        return render_template('app.html')
+    return render_template('app.html')
 
 @app.route('/send', methods=['POST'])
 
 def send(sum=sum):
-        if request.method == 'POST':
-                #pulling data
-                num1 = request.form['num1']
-                num2 = request.form['num2']
-                operation = request.form['operation']
-                
-                #calculating if statements
-                if operation == 'add':
-                        sum = float(num1) + float(num2)
-                        return render_template('app.html', sum=sum)
+    if request.method == 'POST':
+        #pulling data
+        n = request.form['n']
+        k = request.form['l']
+        operation = request.form['operation']
+        
+        #calculating if statements
+        if operation == 'permutace':
+            sum =n
+            return render_template('app.html', sum=sum)
 
-                elif operation == 'substraction':
-                        sum = float(num1) - float(num2)
-                        return render_template('app.html', sum=sum)
+        elif operation == 'variace':
+            sum =n
+            return render_template('app.html', sum=sum)
 
-                elif operation == 'multiply':
-                        sum = float(num1) * float(num2)
-                        return render_template('app.html', sum=sum)
+        elif operation == 'kombinace':
+            sum =n
+            return render_template('app.html', sum=sum)
 
-                elif operation == 'substraction':
-                        sum = float(num1) - float(num2)
-                        return render_template('app.html', sum=sum)
+        elif operation == 'permutacesop':
+            sum =n
+            return render_template('app.html', sum=sum)
 
-                else:
-                        return render_template('app.html')
+        elif operation == 'variacesop':
+            sum =n
+            return render_template('app.html', sum=sum)
+
+        elif operation == 'kombinacesop':
+            sum = n
+            return render_template('app.html', sum=sum)
+
+        else:
+            sum = "Chyba"
+            return render_template('app.html')
