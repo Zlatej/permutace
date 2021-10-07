03bee1ee32c842cdac156fe2fedef87b5d82a24e
Matej Skalicky <matej.skalickyy@gmail.com>
2021-10-07 10:50:56 +0200

    funfuje

--- app.py
+++ app.py
@@ -12,15 +12,38 @@ def main():
 @app.route('/send', methods=['POST'])
 def send(sum=sum):
     if request.method == 'POST':
-        n = int(request.form['n'])
-        k = int(request.form['k'])
-        #r1 = int(request.form['r1'])
-        #r2 = int(request.form['r2'])
-        #r3 = int(request.form['r3'])
-        #r4 = int(request.form['r4'])
-        #r5 = int(request.form['r5'])
         operace = request.form['operace']
 
+        #form
+        if operace == 'permutace':
+            n = int(request.form['n'])
+
+        elif operace == 'variace':
+            n = int(request.form['n'])
+            k = int(request.form['k'])
+
+        elif operace == 'kombinace':
+            n = int(request.form['n'])
+            k = int(request.form['k'])
+
+        elif operace == 'permutacesop':
+            n = int(request.form['n'])
+
+            r1 = int(request.form['r1'])
+            r2 = int(request.form['r2'])
+            r3 = int(request.form['r3'])
+            r4 = int(request.form['r4'])
+            r5 = int(request.form['r5'])
+
+        elif operace == 'variacesop':
+            n = int(request.form['n'])
+            k = int(request.form['k'])
+
+        elif operace == 'kombinacesop':
+            n = int(request.form['n'])
+            k = int(request.form['k'])
+
+        #vypocty
         if operace == 'permutace':
             sum = float(math.factorial(n))
             return render_template('app.html', sum=sum)
@@ -29,20 +52,20 @@ def send(sum=sum):
             sum = float(math.factorial(n)/math.factorial(n-k))
             return render_template('app.html', sum=sum)
 
-        #elif operace == 'kombinace':
-        #    sum = float(math.factorial(n)/math.factorial(r1)/math.factorial(r2)/math.factorial(r3)/math.factorial(r4)/math.factorial(r5))
-        #    return render_template('app.html', sum=sum)
+        elif operace == 'kombinace':
+            sum = float(math.factorial(n)/math.factorial(n - k)/math.factorial(k))
+            return render_template('app.html', sum=sum)
 
         elif operace == 'permutacesop':
-            sum = float(n ** k)
+            sum = float(math.factorial(n)/math.factorial(r1)/math.factorial(r2)/math.factorial(r3)/math.factorial(r4)/math.factorial(r5))
             return render_template('app.html', sum=sum)
 
         elif operace == 'variacesop':
-            sum = float(n) + float(k)
+            sum = float(n ** k)
             return render_template('app.html', sum=sum)
 
         elif operace == 'kombinacesop':
-            sum = float(n) + float(k)
+            sum = float(math.factorial(n + k - 1)/math.factorial(n - 1)/math.factorial(k))
             return render_template('app.html', sum=sum)
 
 
